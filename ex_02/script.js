9e44b576411370b7bd5b72887ae3f59f3e58009f
Roel Gerards <51865136+roelgerards@users.noreply.github.com>
2019-09-03 18:06:18 -0500

    added api_browser exercise files

--- ex_02/script.js
+++
@@ -1,8 +0,0 @@
-var number = 0;
-var i = 0
-for (number; 1000 >= number; number++) {
-
-    if (i !== number % 2) {
-        console.log(number);
-    }
-}
--- js_exercises/ex_130/index130.js
+++ js_exercises/ex_130/index130.js
@@ -2,7 +2,7 @@
 const mutants = 
 ['Professor X','Cyclops','Iceman','Angel','Beast','Phoenix'];
 
-console.log('Original mutants: '+mutants, '\n');
-console.log('Sorted mutants: ' +mutants.sort(), '\n');
-console.log('Reversed mutants:' +mutants.reverse(), '\n');
-console.log('Mutants name separated: ' +mutants.join(' * '));
+console.log('Original mutants: ' + mutants, '\n');
+console.log('Sorted mutants: '  + mutants.sort(), '\n');
+console.log('Reversed mutants:'  + mutants.reverse(), '\n');
+console.log('Mutants name separated: '  + mutants.join(' * '));
