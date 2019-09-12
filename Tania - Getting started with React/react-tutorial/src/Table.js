c9aeb716513e0feab56a889b6f99275bfc518735
Roel Gerards <51865136+roelgerards@users.noreply.github.com>
2019-09-11 22:38:58 -0500

    React commence

---
+++ Tania - Getting started with React/react-tutorial/src/Table.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+    render() {
+      const { characterData } = this.props
+  
+      return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={characterData} />
+        </table>
+      )
+    }
+  }
+
+export default Table
